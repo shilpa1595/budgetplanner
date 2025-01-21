00fb1f819e5a0e1db95ae0adecd686f7fc4cedff
shilpa <shilpapatil15195@gmail.com>
2025-01-21 21:11:41 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path:'budget-planner', loadChildren:()=>import('./budget-planner/budget-planner.module').then(m=>m.BudgetPlannerModule)
+    }
+];
